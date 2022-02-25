d1204159561cb5db48b61009ce8b31d7704db7cc
Paulo Henrique Vieira <30737597+PHVDS@users.noreply.github.com>
2022-02-25 14:53:47 -0300

    TagHelper - Cadastro de Cliente e Atualizando Tabela.

---
+++ LojaVirtual/LojaVirtual/Migrations/20220225174800_AtualizandoTabela.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LojaVirtual.Migrations
+{
+    public partial class AtualizandoTabela : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "NewsletterEmails",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Telefone",
+                table: "Clientes",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Telefone",
+                table: "Clientes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "NewsletterEmails",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
--- LojaVirtual/LojaVirtual/Models/Cliente.cs
+++ LojaVirtual/LojaVirtual/Models/Cliente.cs
@@ -10,8 +10,9 @@ namespace LojaVirtual.Models
 		public int Id { get; set; }
 		public string Nome { get; set; }
 		public DateTime Nascimento { get; set; }
-		public string CPF { get; set; }
 		public string Sexo { get; set; }
+		public string CPF { get; set; }
+		public string Telefone { get; set; }
 		public string Email { get; set; }
 		public string Senha { get; set; }
 	}
