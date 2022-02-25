f765e07bce5e9075ee1421b78ee1e11411afd88c
Paulo Henrique Vieira <30737597+PHVDS@users.noreply.github.com>
2022-02-25 19:52:58 -0300

    Cadastro cliente - Repository

---
+++ LojaVirtual/LojaVirtual/Repositories/Contracts/IClienteRepository.cs
@@ -0,0 +1,21 @@
+﻿using LojaVirtual.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Repositories.Contracts
+{
+	public interface IClienteRepository
+	{
+		Cliente Login(string Email, string Senha);
+
+		void Cadastrar(Cliente cliente);
+		void Atualizar(Cliente cliente);
+		void Excluir(int Id);
+		void ObterCliente(int Id);
+		List<Cliente> ObterTodosClientes();
+
+
+	}
+}
