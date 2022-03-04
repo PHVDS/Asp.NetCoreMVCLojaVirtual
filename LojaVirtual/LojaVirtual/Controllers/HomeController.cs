fa6ed7080d24e2c28428235394af2f6481fd76be
Paulo Henrique Vieira <30737597+PHVDS@users.noreply.github.com>
2022-03-03 23:43:06 -0300

    Formulario de Login

--- LojaVirtual/LojaVirtual/Controllers/HomeController.cs
+++ LojaVirtual/LojaVirtual/Controllers/HomeController.cs
@@ -91,9 +91,23 @@ namespace LojaVirtual.Controllers
 			return View("Contato");
 		}
 
+		[HttpGet]
 		public IActionResult Login()
 		{
 			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Login([FromForm]Cliente cliente)
+		{
+			if (cliente.Email == "paulocraazy@gmail.com" && cliente.Senha == "1234")
+			{
+				return new ContentResult() { Content = "Logado"};
+			}
+			else
+			{
+				return new ContentResult() { Content = "Nao logado" };
+			}
 		}
 
 		[HttpGet]
