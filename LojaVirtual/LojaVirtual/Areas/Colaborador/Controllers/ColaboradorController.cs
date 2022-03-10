ba3734b574608c2d42f4b2069e5c946211cb0bb8
Paulo Henrique Vieira <30737597+PHVDS@users.noreply.github.com>
2022-03-10 18:16:14 -0300

    Area Colaborador - Excluir

--- LojaVirtual/LojaVirtual/Areas/Colaborador/Controllers/ColaboradorController.cs
+++ LojaVirtual/LojaVirtual/Areas/Colaborador/Controllers/ColaboradorController.cs
@@ -72,7 +72,11 @@ namespace LojaVirtual.Areas.Colaborador.Controllers
 		[HttpGet]
 		public IActionResult Excluir(int id)
 		{
-			return View();
+			_colaboradorRepository.Excluir(id);
+
+			TempData["MSG_S"] = Mensagem.MSG_S002;
+
+			return RedirectToAction(nameof(Index));
 		}
 	}
 }
