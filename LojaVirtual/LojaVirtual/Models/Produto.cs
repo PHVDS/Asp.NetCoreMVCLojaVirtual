7991f3898384b6ce9f03bbc811d9c39ea316e568
Paulo Henrique Vieira <30737597+PHVDS@users.noreply.github.com>
2022-03-17 19:26:14 -0300

    Area Colaborador - Modelo Produto

--- LojaVirtual/LojaVirtual/Models/Produto.cs
+++ LojaVirtual/LojaVirtual/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,5 +12,17 @@ namespace LojaVirtual.Models
 		public string Nome { get; set; }
 		public string  Descricao { get; set; }
 		public decimal Valor { get; set; }
+		public int Quantidade { get; set; }
+
+		//Frete - Correios
+		public double Peso { get; set; }
+		public int Largura { get; set; }
+		public int Altura { get; set; }
+		public int Comprimento { get; set; }
+
+		public int CategoriaId { get; set; }
+
+		[ForeignKey("CategoriaId")]
+		public Categoria Categoria { get; set; }
 	}
 }
