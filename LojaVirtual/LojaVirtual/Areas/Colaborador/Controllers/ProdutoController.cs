7c4b4c6c3a6895dadb82d04dd1d5a313ce1b0ac3
Paulo Henrique Vieira <30737597+PHVDS@users.noreply.github.com>
2022-03-17 20:39:27 -0300

    Area Colaborador - Listagem de Produtos

---
+++ LojaVirtual/LojaVirtual/Areas/Colaborador/Controllers/ProdutoController.cs
@@ -0,0 +1,25 @@
+﻿using LojaVirtual.Repositories.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Areas.Colaborador.Controllers
+{
+	[Area("Colaborador")]
+	public class ProdutoController : Controller
+	{
+		private readonly IProdutoRepository _produtoRepository;
+		public ProdutoController(IProdutoRepository produtoRepository)
+		{
+			_produtoRepository = produtoRepository;
+		}
+
+		public IActionResult Index(int? pagina, string pesquisa)
+		{
+			var produtos = _produtoRepository.ObterTodosProdutos(pagina, pesquisa);
+			return View(produtos);
+		}
+	}
+}
