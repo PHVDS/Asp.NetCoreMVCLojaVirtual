5e13ba7b7a181371d3391c663d87d22d8b29cd89
Paulo Henrique Vieira <30737597+PHVDS@users.noreply.github.com>
2022-03-19 15:56:09 -0300

    Upload - Ajax - Deletar Imagem

--- LojaVirtual/LojaVirtual/wwwroot/js/Actions.js
+++ LojaVirtual/LojaVirtual/wwwroot/js/Actions.js
@@ -16,12 +16,30 @@ function AjaxUploadImagemProduto() {
         $(this).parent().find(".input-file").click();
     });
 
+    $(".btn-imagem-excluir").click(function () {
+        var CampoHidden = $(this).parent().find("input[name=imagem]");
+        var Imagem = $(this).parent().find(".img-upload");
+
+        $.ajax({
+            type: "GET",
+            url: "/Colaborador/Imagem/Deletar?caminho=" + CampoHidden.val(),
+            error: function () {
+                
+            },
+            success: function () {
+                Imagem.attr("src", "/img/imagem-padrao.png");        
+            }
+        })
+    });
+
     $(".input-file").change(function () {
         //Formulário de dados via JavaScript
         var Binario = $(this)[0].files[0];
         var Formulario = new FormData();
         Formulario.append("file", Binario);
 
+        var CampoHidden = $(this).parent().find("input[name=imagem]");
+        var Imagem = $(this).parent().find(".img-upload");
         //Requisição Ajax enviado a Formulario
         $.ajax({
             type: "POST",
@@ -33,7 +51,9 @@ function AjaxUploadImagemProduto() {
                 alert("Erro no envio do arquivo!");
             },
             success: function (data) {
-                alert("Arquivo enviado com sucesso! " + data.caminho);
+                var caminho = data.caminho;
+                Imagem.attr("src", caminho);
+                CampoHidden.val(caminho);
             }
         });
     });
