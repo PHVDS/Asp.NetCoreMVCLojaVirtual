5af366777aac3a699ca25e460676549fa17f52b6
Paulo Henrique Vieira <30737597+PHVDS@users.noreply.github.com>
2022-03-18 22:33:06 -0300

    Area Colaborador - Validacao e Tipo - Parte 5

--- LojaVirtual/LojaVirtual/Models/Produto.cs
+++ LojaVirtual/LojaVirtual/Models/Produto.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using LojaVirtual.Libraries.Lang;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -7,21 +8,40 @@ namespace LojaVirtual.Models
 	public class Produto
 	{
 		public int Id { get; set; }
+
+		[Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
 		public string Nome { get; set; }
 
+		[Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
 		[Display(Name ="Descrição")]
 		public string  Descricao { get; set; }
 
+		[Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
 		[Display(Name ="Preço")]
 		public decimal Valor { get; set; }
+
+		[Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
+		[Range(0, 100000, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E006")]
 		public int Quantidade { get; set; }
 
 		//Frete - Correios
+		[Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
+		[Range(0.001, 30, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E006")]
 		public double Peso { get; set; }
+
+		[Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
+		[Range(11, 105, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E006")]
 		public int Largura { get; set; }
+
+		[Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
+		[Range(2, 105, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E006")]
 		public int Altura { get; set; }
+
+		[Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
+		[Range(16, 105, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E006")]
 		public int Comprimento { get; set; }
 
+		[Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
 		[Display(Name ="Categoria")]
 		public int CategoriaId { get; set; }
 
