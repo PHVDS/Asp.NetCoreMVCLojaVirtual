9c5e7882ed877f66c19ce2344eda309b7127e060
Paulo Henrique Vieira <30737597+PHVDS@users.noreply.github.com>
2022-03-29 14:34:07 -0300

    Index - Listagem de Produtos - Ordenação

--- LojaVirtual/LojaVirtual/Models/ViewModels/IndexViewModel.cs
+++ LojaVirtual/LojaVirtual/Models/ViewModels/IndexViewModel.cs
@@ -1,4 +1,6 @@
-﻿using X.PagedList;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using X.PagedList;
 
 namespace LojaVirtual.Models.ViewModels
 {
@@ -6,5 +8,17 @@ namespace LojaVirtual.Models.ViewModels
 	{
 		public NewsletterEmail newsletter { get; set; }
 		public IPagedList<Produto> lista { get; set; }
+		public List<SelectListItem> ordenacao
+		{
+			get
+			{
+				return new List<SelectListItem>() {
+					new SelectListItem("Alfabética", "A"),
+					new SelectListItem("Menor preço", "ME"),
+					new SelectListItem("Maior preço", "MA"),
+				};
+			}
+			private set { }
+		}
 	}
 }
