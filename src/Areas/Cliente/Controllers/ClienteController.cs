c2f659894c0bdc87e7ba3a3e18ad10c33213876a
Paulo Henrique Vieira <30737597+PHVDS@users.noreply.github.com>
2022-05-15 13:18:08 -0300

    Cliente - Minha conta - Atualizar Dados Pessoais.

--- src/Areas/Cliente/Controllers/ClienteController.cs
+++ src/Areas/Cliente/Controllers/ClienteController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LojaVirtual.Libraries.Filtro;
+using LojaVirtual.Libraries.Lang;
+using LojaVirtual.Libraries.Login;
+using LojaVirtual.Repositories;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +12,47 @@ namespace LojaVirtual.Areas.Cliente.Controllers
 {
 	public class ClienteController : Controller
 	{
+		private readonly LoginCliente _loginCliente;
+		private readonly ClienteRepository _clienteRepository;
+
+		public ClienteController(LoginCliente loginCliente, ClienteRepository clienteRepository)
+		{
+			_loginCliente = loginCliente;
+			_clienteRepository = clienteRepository;
+		}
+
+		[ClienteAutorizacao]
 		public IActionResult Index()
 		{
+			return View();
+		}
+		
+		[ClienteAutorizacao]
+		[HttpGet]
+		public IActionResult Atualizar()
+		{
+			Models.Cliente cliente = _clienteRepository.ObterCliente(_loginCliente.GetCliente().Id);
+
+			return View(cliente);
+		}
+
+		[ClienteAutorizacao]
+		[HttpPost]
+		public IActionResult Atualizar(Models.Cliente cliente)
+		{
+			ModelState.Remove("Senha");
+			ModelState.Remove("ConfirmacaoSenha");
+
+			if (ModelState.IsValid)
+			{
+				cliente.Senha = _loginCliente.GetCliente().Senha;
+				_clienteRepository.Atualizar(cliente);
+				_loginCliente.Login(cliente);
+
+				TempData["MSG_S"] = Mensagem.MSG_S001;
+
+				return RedirectToAction(nameof(Index));
+			}
 			return View();
 		}
 	}
