f1fb950e4f3d7132bd70bd83cbaeacf7da50ee37
Paulo Henrique <Peaga17@hotmail.com>
2023-02-24 15:58:56 -0300

    Personalizando - Error 404.

--- src/Controllers/ErrorController.cs
+++ src/Controllers/ErrorController.cs
@@ -6,19 +6,37 @@ namespace LojaVirtual.Controllers
 {
     public class ErrorController : Controller
     {
-        public IActionResult Error500()
-        {
-            var exception = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
-
-            ViewBag.Mensagem = exception.Error.Message;
-            return View();
-        }
-
         [Route("Error/{statusCode}")]
         public IActionResult ErroGenerico(int statusCode)
         {
             ViewBag.Mensagem = string.Format("Código de erro: {0} - Mensagem: {1}", statusCode, ReasonPhrases.GetReasonPhrase(statusCode));
-            return View("Error500");
+            return View();
         }
-    }
+
+		public IActionResult Error500()
+		{
+			var exception = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+			ViewBag.Mensagem = exception.Error.Message;
+			return View();
+		}
+
+        [Route("Error/503")]
+        public IActionResult Error503()
+        {
+            return View();
+        }
+
+		[Route("Error/404")]
+		public IActionResult Error404()
+		{
+			return View();
+		}
+
+		[Route("Error/403")]
+		public IActionResult Error403()
+		{
+			return View();
+		}
+	}
 }
