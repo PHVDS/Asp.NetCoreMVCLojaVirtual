87aae4757c1594d503053f77f52dc5c15a7f0db0
Paulo Henrique <Peaga17@hotmail.com>
2023-02-27 15:16:13 -0300

    Instalando e Configurando Serilog.

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Events;
+using System;
+using System.IO;
 
 namespace LojaVirtual
 {
@@ -8,11 +12,33 @@ namespace LojaVirtual
 		public static void Main(string[] args)
 
 		{
-			CreateWebHostBuilder(args).Build().Run();
+			string caminhoLog = Path.Combine(Directory.GetCurrentDirectory(), "Logs.txt");
+
+			Log.Logger = new LoggerConfiguration()
+				.MinimumLevel
+				.Debug().MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+				.Enrich.FromLogContext()
+				.WriteTo.File(caminhoLog)
+				.CreateLogger();
+
+			try
+			{
+				Log.Information("--- SERVIDOR INICIANDO ---");
+				CreateWebHostBuilder(args).Build().Run();
+			}
+			catch (Exception ex)
+			{
+				Log.Fatal(ex, "--- SERVIDOR ENCERROU INESPERADAMENTE ---");
+			}
+			finally
+			{ 
+				Log.CloseAndFlush();
+			}
 		}
 
 		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
 			WebHost.CreateDefaultBuilder(args)
-				.UseStartup<Startup>();
+				.UseStartup<Startup>()
+				.UseSerilog();
 	}
 }
